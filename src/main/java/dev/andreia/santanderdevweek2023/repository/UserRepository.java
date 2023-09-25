637b5e57ba1568ca255d34501a4dea4a30ba39d8
Andreia-Nunes <andreia.nunes@academico.ifpb.edu.br>
2023-09-24 22:03:52 -0300

    Criação de repositório de usuários

---
+++ src/main/java/dev/andreia/santanderdevweek2023/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package dev.andreia.santanderdevweek2023.repository;
+
+import dev.andreia.santanderdevweek2023.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
